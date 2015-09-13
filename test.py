b700bc53ef890bea2ea7877dd9f9ad17ff5465aa
zhantong1994 <zhantong1994@163.com>
2015-09-13 13:24:32 +0800

    修改获取所有版面代码，增加获取某版面指定起始位置的主题列表

--- test.py
+++ test.py
@@ -6,15 +6,35 @@ headers={
 	'Accept-Encoding': 'deflate, sdch',
 }
 
-s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbsall')
-con=s.read().decode('gb2312')
-root=etree.HTML(con)
-#trs=root.xpath('//tr/td[1]|//tr/td[2]/a|//tr/td[3]|//tr/td[4]/a')
-trs=root.xpath('//tr')
-for tr in trs[1:]:
-	tds=tr.findall('td')
-	no=tds[0].text
-	board=tds[1].find('a').text
-	category=tds[2].text[1:-1]
-	name=tds[3].find('a').text[3:]
-	print(name)
+def get_board_list():
+	s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbsall')
+	con=s.read().decode('gb2312')
+	root=etree.HTML(con)
+	trs=root.xpath('//tr')
+	for tr in trs[1:]:
+		tds=tr.findall('td')
+		no=tds[0].text
+		board=tds[1].find('a').text
+		category=tds[2].text[1:-1]
+		name=tds[3].find('a').text[3:]
+		print(name)
+
+def get_post_list(board_id,start=''):
+	s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbstdoc?board=M_Logistic'+'&start='+start)
+	con=s.read().decode('gb2312')
+	root=etree.HTML(con)
+	trs=root.xpath('//tr')
+	for tr in trs[1:]:
+		tds=tr.findall('td')
+		if not tds[0].text:
+			continue
+		no=tds[0].text
+		author=tds[2].find('a').text
+		date=tds[3].text
+		a_title=tds[4].find('a')
+		id=a_title.get('href').rsplit('=',1)[-1]
+		title=a_title.text[2:]
+		print(id)
+
+if __name__=='__main__':
+	get_post_list('aaa')
