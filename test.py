93bf1578628309a643ba0b4837637678fa239c69
zhantong1994 <zhantong1994@163.com>
2015-09-13 19:50:21 +0800

    将非常相似的html请求部分单独做成函数
    并且将代码修改为面向对象

--- test.py
+++ test.py
@@ -13,51 +13,54 @@ headers={
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
 	'Accept-Encoding': 'deflate, sdch',
 }
+class NJU_BBS():
+	def get_html(self,url):
+		get=urllib.request.urlopen(url)
+		content=get.read().decode('gb2312')
+		root=etree.HTML(content)
+		return root
+	def get_board_list(self):
+		url='http://bbs.nju.edu.cn/bbsall'
+		root=self.get_html(url)
+		trs=root.xpath('//tr')
+		for tr in trs[1:]:
+			tds=tr.findall('td')
+			no=tds[0].text
+			board=tds[1].find('a').text
+			category=tds[2].text[1:-1]
+			name=tds[3].find('a').text[3:]
+			print(name)
 
-def get_board_list():
-	s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbsall')
-	con=s.read().decode('gb2312')
-	root=etree.HTML(con)
-	trs=root.xpath('//tr')
-	for tr in trs[1:]:
-		tds=tr.findall('td')
-		no=tds[0].text
-		board=tds[1].find('a').text
-		category=tds[2].text[1:-1]
-		name=tds[3].find('a').text[3:]
-		print(name)
+	def get_post_list(self,board,start=''):
+		url='http://bbs.nju.edu.cn/bbsdoc?&type=doc'+'&board='+board+'&start='+start
+		root=self.get_html(url)
+		trs=root.xpath('//tr')
+		for tr in trs[1:]:
+			tds=tr.findall('td')
+			if tds[0].text==None or not tds[0].text.isdigit():
+				continue
+			no=tds[0].text
+			author=tds[2].find('a').text
+			date=tds[4].find('nobr').text
+			a_title=tds[4].find('nobr/td/a')
+			t=url_split.findall(a_title.get('href'))[0]
+			board=t[0]
+			file=t[1]
+			num=t[2]
+			title=a_title.text
+			print(title)
 
-def get_post_list(board_id,start=''):
-	s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbsdoc?board=M_Logistic&type=doc'+'&start='+start)
-	con=s.read().decode('gb2312')
-	root=etree.HTML(con)
-	trs=root.xpath('//tr')
-	for tr in trs[1:]:
-		tds=tr.findall('td')
-		if tds[0].text==None or not tds[0].text.isdigit():
-			continue
-		no=tds[0].text
-		author=tds[2].find('a').text
-		date=tds[4].find('nobr').text
-		a_title=tds[4].find('nobr/td/a')
-		t=url_split.findall(a_title.get('href'))[0]
-		board=t[0]
-		file=t[1]
-		num=t[2]
-		title=a_title.text
-		print(title)
-
-def get_post():
-	s=urllib.request.urlopen('http://bbs.nju.edu.cn/bbscon?board=M_Job&file=M.1441416413.A&num=11981')
-	con=s.read().decode('gb2312')
-	root=etree.HTML(con)
-	content=root.find('.//textarea').text
-	con=content_split.findall(content)[0]
-	ip=modify.findall(content)
-	gid=root.find('.//center/a[4]').get('href').rsplit('=',1)[-1]
-	dl_list=download.findall(content)
-	print(dl_list)
+	def get_post(self,board,file,num=''):
+		url='http://bbs.nju.edu.cn/bbscon?board='+board+'&file='+file+'&num='+num
+		root=self.get_html(url)
+		content=root.find('.//textarea').text
+		con=content_split.findall(content)[0]
+		ip=modify.findall(content)
+		gid=root.find('.//center/a[4]').get('href').rsplit('=',1)[-1]
+		dl_list=download.findall(content)
+		print(dl_list)
 
 
 if __name__=='__main__':
-	get_post()
+	bbs=NJU_BBS()
+	bbs.get_board_list()
